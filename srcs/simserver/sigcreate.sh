78513b3f9f4ff7b1ab55eefd3d33a6d2874ae072
Tetsunari Inamura <inamura@nii.ac.jp>
2014-10-30 19:18:56 +0900

    Add -Wall option

--- srcs/simserver/sigcreate.sh
+++ srcs/simserver/sigcreate.sh
@@ -62,7 +62,7 @@ all: \$(OBJS)
 
 #compile
 ./%.so: ./%.cpp
-	g++ -DCONTROLLER -DNDEBUG -DUSE_ODE -DdDOUBLE -I\$(SIG_SRC) -I\$(SIG_SRC)/comm/controller -fPIC -shared -o \$@ $<
+	g++ -Wall -DCONTROLLER -DNDEBUG -DUSE_ODE -DdDOUBLE -I\$(SIG_SRC) -I\$(SIG_SRC)/comm/controller -fPIC -shared -o \$@ $<
 
 clean:
 	rm ./*.so
