5728b757f42e7a4706f0bfc43ba31c9396f5883c
tome-yamada <bombe@cpost.plala.or.jp>
2015-05-13 16:52:26 +0900

    Return to symbolic link

--- srcs/server/commonlib/comm/controller/Action.h
+++ srcs/server/commonlib/comm/controller/Action.h
@@ -1,16 +1,1 @@
-/* $Id: Action.h,v 1.1.1.1 2011-03-25 02:18:50 okamoto Exp $ */ 
-#ifndef CommController_Action_h
-#define CommController_Action_h
-
-#include "Controller.h"
-
-class ActionEvent : public ControllerEvent
-{
-public:
-	bool set(int packetNum, int seq, char *data, int n);
-};
-
-
-#endif // CommController_Action_h
- 
-
+../../../../commonlib/comm/controller/Action.h
