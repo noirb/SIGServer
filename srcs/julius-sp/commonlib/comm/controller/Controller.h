949d535ad47ede80733f75d7affd4a8aba424600
tome-yamada <bombe@cpost.plala.or.jp>
2015-05-13 16:34:39 +0900

    Return to symbolic link

--- srcs/julius-sp/commonlib/comm/controller/Controller.h
+++ srcs/julius-sp/commonlib/comm/controller/Controller.h
@@ -1,18 +1,1 @@
-/* $Id: Controller.h,v 1.1.1.1 2011-03-25 02:18:50 okamoto Exp $ */ 
-#ifndef CommController_Controller_h
-#define CommController_Controller_h
-
-class ControllerEvent
-{
-protected:
-	double m_currTime; 
-
-public:
-	double time() { return m_currTime; }
-
-	void setTime(double time) { m_currTime = time; }
-};
-
-#endif // CommController_Controller_h
- 
-
+../../../../commonlib/comm/controller/Controller.h
