ca2c340b38065d06374e3606b6d41d12ff34974c
tome-yamada <bombe@cpost.plala.or.jp>
2015-05-13 15:24:48 +0900

    (1)Remove operator.(2)Remove typedef that are too omitted.(3)Remove meaningless code.(4)Change tab to space.

--- srcs/model/samples/export/onCollision.cpp
+++ srcs/model/samples/export/onCollision.cpp
@@ -4,16 +4,18 @@
 
 class AgentController: public Controller {
 public:
-	double	onAction(ActionEvent &evt);
-	void	onCollision(CollisionEvent &evt);
+	double onAction(ActionEvent &evt);
+	void   onCollision(CollisionEvent &evt);
 };
 
 
-void AgentController::onCollision(CollisionEvent &evt) {
+void AgentController::onCollision(CollisionEvent &evt)
+{
 	LOG_MSG(("%s : onCollision() ", myname()));
-	typedef CollisionEvent::WithC C;
-	const C & with = evt.getWith();
-	for (C::const_iterator i=with.begin(); i!=with.end(); i++) {
+
+	const std::vector<std::string> & with = evt.getWith();
+
+	for (std::vector<std::string>::const_iterator i=with.begin(); i!=with.end(); i++) {
 		std::string s = *i;
 		LOG_MSG(("\"%s\"", s.c_str()));
 	}
--- srcs/model/samples/export/scMove.cpp
+++ srcs/model/samples/export/scMove.cpp
@@ -9,89 +9,91 @@
 class MoveController : public Controller
 {
 public:
-  enum STATE {
-	IDLE,
-	WALKING
-  };
+	enum STATE
+	{
+		IDLE,
+		WALKING
+	};
 
-  double onAction(ActionEvent &evt);
-  void onRecvMessage(RecvMessageEvent &evt);
-  STATE getState() { return m_state; }
-  void setState(STATE state) { m_state = state; }
+	double onAction(ActionEvent &evt);
+	void onRecvMessage(RecvMessageEvent &evt);
+	STATE getState() { return m_state; }
+	void setState(STATE state) { m_state = state; }
 
 private:
-  STATE m_state;
-  double m_xDest, m_yDest, m_zDest;
+	STATE m_state;
+	double m_xDest, m_yDest, m_zDest;
 };
 
 #define PI 3.14159265359
 
 double MoveController::onAction(ActionEvent &evt)
 {
-  static int cnt = 0;
+	static int cnt = 0;
 
-  try {
-	const char *name = myname();
-	SimObj *obj = getObj(name);
-	if (obj && !obj->dynamics()) {
+	try {
+		const char *name = myname();
+		SimObj *obj = getObj(name);
+		if (obj && !obj->dynamics()) {
 
-	  // my position
-	  double x = obj->x();
-	  double y = obj->y();
-	  double z = obj->z();
+			// my position
+			double x = obj->x();
+			double y = obj->y();
+			double z = obj->z();
 
-	  switch (m_state)
-	{
-	case IDLE:
-	  break;
+			switch (m_state)
+			{
+				case IDLE:
+					break;
 
-	case WALKING:
-	  {
-		// calc direction to target
-		double xDir = m_xDest - x;
-		double yDir = m_yDest - y;
-		double zDir = m_zDest - z;
+				case WALKING:
+				{
+					// calc direction to target
+					double xDir = m_xDest - x;
+					double yDir = m_yDest - y;
+					double zDir = m_zDest - z;
 
-		double distance = sqrt(xDir*xDir+yDir*yDir+zDir*zDir);
-		if (fabs(distance) < 0.000001) {
-		  LOG_MSG(("*** arrived to destination(%f %f %f) ***", m_xDest, m_yDest, m_zDest));
-		  setState(IDLE);
-		  break;
-		} if (fabs(distance) < 1) {
-		  // calc new position
-		  double speed = 0.5;
-		  x = x + (xDir * speed);
-		  y = y + (yDir * speed);
-		  z = z + (zDir * speed);
-		} else {
-		  // normalize direction (only when distance >= 1)
-		  xDir = xDir / distance;
-		  yDir = yDir / distance;
-		  zDir = zDir / distance;
+					double distance = sqrt(xDir*xDir+yDir*yDir+zDir*zDir);
 
-		  // calc new position
-		  double speed = 0.5;
-		  x = x + (xDir * speed);
-		  y = y + (yDir * speed);
-		  z = z + (zDir * speed);
+					if (fabs(distance) < 0.000001) {
+						LOG_MSG(("*** arrived to destination(%f %f %f) ***", m_xDest, m_yDest, m_zDest));
+						setState(IDLE);
+						break;
+					}
+					if (fabs(distance) < 1) {
+						// calc new position
+						double speed = 0.5;
+						x = x + (xDir * speed);
+						y = y + (yDir * speed);
+						z = z + (zDir * speed);
+					} else {
+						// normalize direction (only when distance >= 1)
+						xDir = xDir / distance;
+						yDir = yDir / distance;
+						zDir = zDir / distance;
+
+						// calc new position
+						double speed = 0.5;
+						x = x + (xDir * speed);
+						y = y + (yDir * speed);
+						z = z + (zDir * speed);
+					}
+
+					// set new position and angle
+					obj->setPosition(x, y, z);
+					LOG_MSG(("pos(%f %f %f) distance(%f)", x, y, z, distance))
+				}
+				break;
+			}
 		}
-
-		// set new position and angle
-		obj->setPosition(x, y, z);
-		LOG_MSG(("pos(%f %f %f) distance(%f)", x, y, z, distance))
-		  }
-	  break;
-	}
+	} catch(SimObj::NoAttributeException &) {
+	} catch(SimObj::AttributeReadOnlyException &) {
+	} catch(SimObj::Exception &) {
 	}
 
-  } catch(SimObj::NoAttributeException &) {
-  } catch(SimObj::AttributeReadOnlyException &) {
-  } catch(SimObj::Exception &) {
-  }
+	cnt++;
 
-  cnt++;
-
-  return 0.1;
+	return 0.1;
 }
 
 void MoveController::onRecvMessage(RecvMessageEvent &evt)
@@ -101,45 +103,46 @@ void MoveController::onRecvMessage(RecvMessageEvent &evt)
 
 	int n = evt.getSize();
 	LOG_MSG(("n = %d", n));
+
 	if (n > 0) {
-	  //	  std::string receiver = evt.getString(0);
-	  std::string cmd = evt.getString(0);
-	  LOG_MSG(("cmd = %s", cmd.c_str()));
+		//	  std::string receiver = evt.getString(0);
+		std::string cmd = evt.getString(0);
+		LOG_MSG(("cmd = %s", cmd.c_str()));
 
-	  if (strcmp(cmd.c_str(), "move")==0) {
-	// -----------------------------------------
-	//	move to destination point
-	// -----------------------------------------
-	double x = atof(evt.getString(1));
-	double y = atof(evt.getString(2));
-	double z = atof(evt.getString(3));
-	LOG_MSG(("goto (%f, %f, %f)", x, y, z));
+		if (strcmp(cmd.c_str(), "move")==0) {
+			// -----------------------------------------
+			//  move to destination point
+			// -----------------------------------------
+			double x = atof(evt.getString(1));
+			double y = atof(evt.getString(2));
+			double z = atof(evt.getString(3));
+			LOG_MSG(("goto (%f, %f, %f)", x, y, z));
 
-	// set destination point
-	m_xDest = x;
-	m_yDest = y;
-	m_zDest = z;
+			// set destination point
+			m_xDest = x;
+			m_yDest = y;
+			m_zDest = z;
 
-	// set new state
-	m_state = WALKING;
-	  }
-	  else if (strcmp(cmd.c_str(), "print")==0) {
-	if (obj) {
-	  double vpx = obj->vpx();
-	  double vpy = obj->vpy();
-	  double vpz = obj->vpz();
+			// set new state
+			m_state = WALKING;
+		}
+		else if (strcmp(cmd.c_str(), "print")==0) {
+			if (obj) {
+				double vpx = obj->vpx();
+				double vpy = obj->vpy();
+				double vpz = obj->vpz();
 
-	  double vvx = obj->vvx();
-	  double vvy = obj->vvy();
-	  double vvz = obj->vvz();
+				double vvx = obj->vvx();
+				double vvy = obj->vvy();
+				double vvz = obj->vvz();
 
-	  LOG_MSG(("vp(%f, %f, %f) vv(%f, %f, %f)", vpx, vpy, vpz, vvx, vvy, vvz));
-	}
-	  }
+				LOG_MSG(("vp(%f, %f, %f) vv(%f, %f, %f)", vpx, vpy, vpz, vvx, vvy, vvz));
+			}
+		}
 	}
 }
 
-extern "C"	Controller * createController ()
+extern "C" Controller * createController ()
 {
 	return new MoveController;
 }
