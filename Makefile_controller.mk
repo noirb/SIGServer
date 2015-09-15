6cd11261a60d9c142597a426cdad2961519313f1
tome-yamada <yamada4tec@gmail.com>
2015-09-15 19:45:30 +0900

    Made makefiles.

---
+++ Makefile_controller.mk
@@ -0,0 +1,63 @@
+#---------------------------------------------------------------
+# User setting
+#---------------------------------------------------------------
+
+FILE_NAME           = Makefile_controller.mk
+SRC_DIR             = ./srcs
+COMMONLIB_DIR       = ./srcs/sigverse/commonlib
+CONTROLLER_SRCS     = ./srcs/sigverse/model/ControllerLib.cpp ./srcs/sigverse/model/runmain.cpp
+JAVA_LIB            = /usr/lib/jvm/java-6-openjdk-amd64/jre/lib/amd64/server
+
+
+OUTDIR   = ./release
+TARGET   = $(OUTDIR)/sigrunac
+INCLUDES = -I$(SRC_DIR)
+LDFLAGS  = -lm -ldl -lode -lpthread -lxerces-c -L$(OUTDIR) -lsigverse-x3d -L$(JAVA_LIB) -ljvm 
+NOMAKEDIR= .git% 
+OBJDIR   = $(OUTDIR)/model
+
+GCC = g++
+CFLAGS = -MMD -MP -std=c++11 -D__cplusplus=201103L -DdDOUBLE -DUSE_ODE -DUSE_XERCES -DSTRICT_INTERFACE -DCONTROLLER
+
+#---------------------------------------------------------------
+# Don't change the following
+#---------------------------------------------------------------
+
+CPPS = $(shell find $(COMMONLIB_DIR)/* $(CONTROLLER_SRCS) -name *.cpp )
+SRCS = $(filter-out $(NOMAKEDIR), $(CPPS))
+DIRS = $(dir $(SRCS))
+BINDIRS = $(addprefix $(OBJDIR)/, $(DIRS))
+
+OBJS = $(addprefix $(OBJDIR)/, $(patsubst %.cpp, %.o, $(SRCS)))
+DEPS = $(OBJS:.o=.d)
+TILS = $(patsubst %.cpp, %.cpp~, $(SRCS))
+
+ifeq "$(strip $(OBJDIR))" ""
+	OBJDIR = .
+endif
+
+ifeq "$(strip $(DIRS))" ""
+	OBJDIR = .
+endif
+
+default:
+	@[ -d  $(OBJDIR)   ] || mkdir -p $(OBJDIR)
+	@[ -d "$(BINDIRS)" ] || mkdir -p $(BINDIRS)
+	@make -f $(FILE_NAME) all --no-print-directory
+
+all : $(TARGET)
+
+#print :
+#	echo $(patsubst %.cpp, %.o, $(SRCS))
+	
+$(TARGET): $(OBJS) 
+	$(GCC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
+
+$(OBJDIR)/%.o: %.cpp
+	$(GCC) $(CFLAGS) $(INCLUDES) -o $@ -c $<
+
+clean:
+	@rm -rf $(TARGET) $(TILS) $(OBJDIR)
+
+-include $(DEPS)
+
---
+++ Makefile_server.mk
@@ -0,0 +1,63 @@
+#---------------------------------------------------------------
+# User setting
+#---------------------------------------------------------------
+
+FILE_NAME           = Makefile_server.mk
+SRC_DIR             = ./srcs
+COMMONLIB_DIR       = ./srcs/sigverse/commonlib
+SERVER_DIR          = ./srcs/sigverse/simserver
+JAVA_LIB            = /usr/lib/jvm/java-6-openjdk-amd64/jre/lib/amd64/server
+
+
+OUTDIR   = ./release
+TARGET   = $(OUTDIR)/sigserver
+INCLUDES = -I$(SRC_DIR)
+LDFLAGS  = -lm -ldl -lode -lpthread -lxerces-c -L$(OUTDIR) -lsigverse-x3d -L$(JAVA_LIB) -ljvm 
+NOMAKEDIR= .git% 
+OBJDIR   = $(OUTDIR)/simserver
+
+GCC = g++
+CFLAGS = -MMD -MP -std=c++11 -D__cplusplus=201103L -DdDOUBLE -DUSE_ODE -DUSE_XERCES -DSTRICT_INTERFACE
+
+#---------------------------------------------------------------
+# Don't change the following
+#---------------------------------------------------------------
+
+CPPS = $(shell find $(COMMONLIB_DIR)/* $(SERVER_DIR)/* -name *.cpp )
+SRCS = $(filter-out $(NOMAKEDIR), $(CPPS))
+DIRS = $(dir $(SRCS))
+BINDIRS = $(addprefix $(OBJDIR)/, $(DIRS))
+
+OBJS = $(addprefix $(OBJDIR)/, $(patsubst %.cpp, %.o, $(SRCS)))
+DEPS = $(OBJS:.o=.d)
+TILS = $(patsubst %.cpp, %.cpp~, $(SRCS))
+
+ifeq "$(strip $(OBJDIR))" ""
+	OBJDIR = .
+endif
+
+ifeq "$(strip $(DIRS))" ""
+	OBJDIR = .
+endif
+
+default:
+	@[ -d  $(OBJDIR)   ] || mkdir -p $(OBJDIR)
+	@[ -d "$(BINDIRS)" ] || mkdir -p $(BINDIRS)
+	@make -f $(FILE_NAME) all --no-print-directory
+
+all : $(TARGET)
+
+#print :
+#	echo $(patsubst %.cpp, %.o, $(SRCS))
+	
+$(TARGET): $(OBJS) 
+	$(GCC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
+
+$(OBJDIR)/%.o: %.cpp
+	$(GCC) $(CFLAGS) $(INCLUDES) -o $@ -c $<
+
+clean:
+	@rm -rf $(TARGET) $(TILS) $(OBJDIR)
+
+-include $(DEPS)
+
