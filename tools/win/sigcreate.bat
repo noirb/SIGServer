6b03970b09bf792f253ef2d5791c587d9d2f71bf
tome-yamada <yamada4tec@gmail.com>
2015-10-22 13:04:42 +0900

    Modify CMake structure.

--- tools/win/sigcreate.bat
+++ tools/win/sigcreate.bat
@@ -1,17 +1,17 @@
 @echo off
 
 if "%1" == "" (
-    set TARGET_DIR=%~dp0..\work
+    set TARGET_DIR="%~dp0"..\work
 ) else (
     set TARGET_DIR=%1
 )
 
 mkdir %TARGET_DIR%
 
-copy %~dp0sigmake.bat       %TARGET_DIR%
-copy %~dp0sigserver.bat     %TARGET_DIR%
-copy %~dp0WinController.cpp %TARGET_DIR%
-copy %~dp0WinWorld.xml      %TARGET_DIR%
+copy "%~dp0"sigmake.bat       %TARGET_DIR%
+copy "%~dp0"sigserver.bat     %TARGET_DIR%
+copy "%~dp0"WinController.cpp %TARGET_DIR%
+copy "%~dp0"WinWorld.xml      %TARGET_DIR%
 
 @echo on
 
